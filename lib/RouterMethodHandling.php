b161359e717c81d5c1e48b529eb8ca55e258056b
Roger Gee <roger.gee@tulsalibrary.org>
2018-07-11 08:14:22 -0500

    Add RouterMethodHandling trait
    Adds a trait to the library that allows for routing to particular class
    methods (either instance or static). The handler descriptor can now have
    the form "CLASS::METHOD". An arbitrary class is acceptable in this case.

---
+++ lib/RouterMethodHandling.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * RouterMethodHandling.php
+ *
+ * tccl/router
+ */
+
+namespace TCCL\Router;
+
+/**
+ * RouterMethodHandling
+ *
+ * Provides routing to a particular method within a request handler
+ * implementation. Route request handler descriptions can now have the form:
+ *
+ *   ClassName::MethodName
+ *
+ * To indicate a static method, use the form:
+ *
+ *   @ClassName::MethodName
+ */
+trait RouterMethodHandling {
+    private $handlerMethod;
+
+    /**
+     * Gets the method identified by the current route handler description.
+     *
+     * @return string
+     *  NOTE: null is returned if no handler method was identified.
+     */
+    public function getHandlerMethod() {
+        return $this->handlerMethod;
+    }
+
+    /**
+     * Overrides Router::createHandler().
+     */
+    protected function createHandler($handler) {
+        if (is_string($handler)) {
+            $parts = @explode('::',$handler,2);
+
+            if (count($parts) == 2) {
+                list($class,$method) = $parts;
+                $this->handlerMethod = $method;
+
+                if ($class[0] != '@') {
+                    $class = new $class;
+                }
+                else {
+                    $class = substr($class,1);
+                }
+
+                return [$class,$method];
+            }
+        }
+
+        return parent::createHandler($handler);
+    }
+}
